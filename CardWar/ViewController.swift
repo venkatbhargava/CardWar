559536c562f86080d26464a26c57b77c1ebe8142
Venkata Bhargav <bhargav.venkata.a@gmail.com>
2020-06-25 11:00:38 +0530

    CardWars app game finish design user interface and basic game logic

--- CardWar/ViewController.swift
+++ CardWar/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var playerScoreLabel: UILabel!
+    @IBOutlet weak var cpuScoreLabel: UILabel!
+    
+    var playerScore: Int = 0
+    var cpuScore: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func dealTapped(_ sender: UIButton) {
+        
+        let leftImageNo = Int.random(in: 2...14)
+        let rightImageNo = Int.random(in: 2...14)
+        
+        print("Deal tapped")
+        leftImageView.image = UIImage(named: "card\(leftImageNo)")
+        rightImageView.image = UIImage(named: "card\(rightImageNo)")
+        
+        if leftImageNo > rightImageNo {
+           
+            playerScore += 1
+            playerScoreLabel.text = String(playerScore)
+        } else {
+            
+            cpuScore += 1
+            cpuScoreLabel.text = String(cpuScore)
+        }
+    }
+    
 }
 
